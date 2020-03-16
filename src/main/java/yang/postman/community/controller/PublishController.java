f35c3bb093f8c03dcfa098ef01cbb35cf258f2c8
xih-yang <320299897@qq.com>
2020-03-16 18:39:15 +0800

    renamed publishController

--- src/main/java/yang/postman/community/controller/PublishController.java
+++ src/main/java/yang/postman/community/controller/PublishController.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
  * @Description : yang.postman.community.controller
  */
 @Controller
-public class PublishControoler {
+public class PublishController {
     @Resource
     private UserMapper userMapper;
     @Resource
